c55f4640717b003bc848e868cb1f4b58485660ec
Ezequiel <emiranda@routeatlas.com>
2015-07-06 12:02:26 -0300

    add update method on products admin section

--- public/dist/js/admin/products.js
+++ public/dist/js/admin/products.js
@@ -1,17 +1,22 @@
-$('#add-products-btn').on('click',function(e){
+$('#add-products-btn').on('click',function(e) {
+  $('#products-form').attr('method', 'POST');
+  $('#products-form').attr('action', '/api/products'); 
   $("#products-box").removeClass('hidden');
 });
 
-$('#cancel-products-btn').on('click',function(e){
+$('#cancel-products-btn').on('click',function(e) {
   $("#products-box").addClass('hidden');
 });
 
 // add
 $('#products-form').on('submit',function(e){  
-  var formData = $(this).serialize();   
+  var formData = $(this).serialize();  
+  var method = $(this).attr('method');
+  var action = $(this).attr('action');
+    
   $.ajax({
-    type: 'POST',
-    url: '/api/products',
+    type: method,
+    url: action,
     data: formData,
     dataType: 'json',
     encode: true,
@@ -36,6 +41,35 @@ $('.delete-product-btn').on('click', function(e){
     },
     complete: function(xhr) {        
       window.location = xhr.url; 
+    }
+  });
+  e.preventDefault();
+});
+
+
+// update
+$('.update-product-btn').on('click', function(e){    
+  var id = $(this).data('item-id');
+  var formElements = $('#products-form')[0].elements;
+
+  $.ajax({
+    url: '/api/products/'+ id,
+    type: 'GET',
+    error: function(err) {      
+      console.error(err);
+    },
+    success: function(response) {      
+      // populate form
+      for (i in response.data) {        
+        if (i in formElements) {
+          formElements[i].value = response.data[i];
+        }
+      }
+    },
+    complete: function(xhr) {
+      $('#products-form').attr('method', 'PUT');
+      $('#products-form').attr('action', '/api/products/' + id); 
+      $("#products-box").removeClass('hidden');
     }
   });
   e.preventDefault();
