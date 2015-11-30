f2f57126d14d30af6307175e3229143fa64a8d1a
Daniel Lopez Monagas <zokeber@gmail.com>
2015-11-29 22:19:49 -0300

    Add custom settings in production mode

--- config/database.js
+++ config/database.js
@@ -2,11 +2,14 @@
 
 var mongodb_user = process.env.MONGODB_USER;
 var mongodb_passwd = process.env.MONGODB_PASSWD;
+var mongodb_server = process.env.MONGODB_HOST;
+var mongodb_port = process.env.MONGODB_PORT;
+var mongodb_name = process.env.MONGODB_NAME;
 
 module.exports = {
 
   db: {
-    production: 'mongodb://' + mongodb_user + ':' + mongodb_passwd + '@127.0.0.1:27017/elchango_prod',
+    production: 'mongodb://' + mongodb_user + ':' + mongodb_passwd + '@' + mongodb_server + ':' + mongodb_port + '/' + mongodb_name,
     development: 'mongodb://localhost:27017/elchango_dev',
     test:        'mongodb://localhost:27017/elchango_test'
   }
